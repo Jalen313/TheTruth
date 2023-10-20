a63a62947ba2dd919d1baf087ff8c1ec7e5ff713
paytriot45 <146483386+paytriot45@users.noreply.github.com>
2023-10-20 12:53:53 -0400

    update3

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -13,5 +13,11 @@ func chapterTwo() {
     print("The smell of coffee waifed into the elevator behind him.")
     print("The ride to the Academy door was shorter than usual as his mind drifted to thoughts of the weekend and last nights cruible of affluence. ")
     print ("As he powered up his iphone to use the attendence monitor app, he noticed that even fewer students than normal where in the main space.")
-    print("")
+    print("He swipes his phone across the scanner qiuckly.")
+    print("Waiting on an audible chime that never came, Spires gazes at the screen of his work supplied iphone.")
+    print("A coundown timer replaced the normal home screen.")
+    print("Infact the countdown timer was now present on all of screens in the academy.")
+    print("Stunned, Spires checks his phone again.")
+    print("The timer seemed to be counting down from 60 minutes.")
+    print("It read 58 minutes 30 seconds and matched the rest og the screens he could see from the foyer.")
 }
