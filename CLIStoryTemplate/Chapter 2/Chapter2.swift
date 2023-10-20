3955ab1e40d6a67d07b6ad57b9eb2e5b60bd453d
paytriot45 <146483386+paytriot45@users.noreply.github.com>
2023-10-20 12:35:26 -0400

    update

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -7,5 +7,9 @@ import Foundation
 
 func chapterTwo() {
     // Your portion of the story goes here
-    print("Pop, lock and drop it.")
+    print("Chapter 2")
+    print("The familiar smell of coffee filled the lobby.")
+    print("Spires presented his security badge and entered the turnstyle, as ususual.")
+    print("The smell of coffee waifed into the elevator behind him.")
+    print ("")
 }
