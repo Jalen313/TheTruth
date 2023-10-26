c698f8d7cd07d1e4a162e823baee3007b7bf2f42
The Truth <alexuswilson541@gmail.com>
2023-10-26 14:23:17 -0400

    chapter 4 print statements

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -6,6 +6,19 @@
 import Foundation
 
 func chapterFour() {
-    // Your portion of the story goes here
-    print("Work")
+    //
+    let NameString = ("Foster")
+    let Name2String = ("Spires")
+    let Name3String = ("Alexus")
+    let _: Int = 80
+    
+    chapterfour()
+    
+    func chapterfour() {
+        print("Foster and Spires asked a mentor Alex how to get into the servers and disarm the virus.")
+        print("Alex gave the keycard to Alexus; but Alexus  is currently busy trying to decode \(80) different codes.")
+        print("So she handed the keycard to Foster and Spires.")
+        print("Leaving it up to them to remember 1 code to save the academy.")
+    }
+
 }
