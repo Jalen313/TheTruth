2b3491879743a4e886f2390cf3d633ded0bcd626
paytriot45 <146483386+paytriot45@users.noreply.github.com>
2023-10-27 15:00:37 -0400

    function update11

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -5,19 +5,25 @@
 
 import Foundation
 
-func chapterTwo() {
-    // Your portion of the story goes here
-    print("Chapter 2")
-    print("The familiar smell of coffee filled the lobby.")
-    print("Spires presented his security badge and entered the turnstyle, as ususual.")
-    print("The smell of coffee waifed into the elevator behind him.")
-    print("The ride to the Academy door was shorter than usual as his mind drifted to thoughts of the weekend and last nights cruible of affluence. ")
-    print ("As he powered up his iphone to use the attendence monitor app, he noticed that even fewer students than normal where in the main space.")
-    print("He swipes his phone across the scanner qiuckly.")
-    print("Waiting on an audible chime that never came, Spires gazes at the screen of his work supplied iphone.")
-    print("A coundown timer replaced the normal home screen.")
-    print("Infact the countdown timer was now present on all of screens in the academy.")
-    print("Stunned, Spires checks his phone again.")
-    print("The timer seemed to be counting down from 60 minutes.")
-    print("It read 58 minutes 30 seconds and matched the rest of the screens he could see from the foyer.")
+func chapterTwo(){
+    func listByAdding(item: String, toList: String) -> String {
+        return toList + "\n" + item
+    }
+    
+    var list = "Chapter 2"
+    list = listByAdding(item:"The familiar smell of coffee filled the lobby.", toList: list)
+    list = listByAdding(item:"Spires presented his security badge and entered the turnstyle, as ususual.", toList: list)
+    list = listByAdding(item:"The smell of coffee waifed into the elevator behind him.", toList: list)
+    list = listByAdding(item:"The ride to the Academy door was shorter than usual as his mind drifted to thoughts of the weekend and last nights cruible of affluence.", toList: list)
+    list = listByAdding(item:"As he powered up his iphone to use the attendence monitor app, he noticed that even fewer students than normal where in the main space.", toList: list)
+    list = listByAdding(item:"He swipes his phone across the scanner qiuckly.", toList: list)
+    list = listByAdding(item:"Waiting on an audible chime that never came, Spires gazes at the screen of his academy supplied iphone.", toList: list)
+    list = listByAdding(item:"A coundown timer replaced the normal home screen.", toList: list)
+    list = listByAdding(item:"Infact the countdown timer was now present on all of screens in the academy.", toList: list)
+    list = listByAdding(item:"Stunned, Spires checks his phone again.", toList: list)
+    list = listByAdding(item:"The timer seemed to be counting down from 60 minutes.", toList: list)
+    list = listByAdding(item:"It read 58 minutes 30 seconds and matched the rest of the screens he could see from the foyer.", toList: list)
+  print("list")
+    
+    
 }
