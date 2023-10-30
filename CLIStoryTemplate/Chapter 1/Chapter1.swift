4322afb1e310ff1807fab3aa474ae724c5c74b0c
Gas <foster@fosterlancastermusic.com>
2023-10-30 16:21:03 -0400

    I added arrays

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -9,14 +9,14 @@ let Name:String = "Foster"
 let Age:Int = 30
 let MinutesLate:Double = 22.5
 let Spy:Bool = true
-var Job:String = "Not A Spy"
+var Job: String = "Not A Spy"
 let HeroName: String = "Spires"
 let Description: String = "Spires was a better spy than Foster and was always looking for answers to life's mysteries."
+var elevatorFloorList: [String] = ["Floor One", "Floor Two", "Floor Three"]
+var waitingList:String = "Waiting..."
 
-var list:String = "Waiting..."
-
-func listByAdding(item: String, toList: String) -> String {
-    return toList + "\n" + item
+func listByAdding(item: String, towaitingList: String) -> String {
+    return towaitingList + "\n" + item
 }
 
 func finishChapter() {
@@ -27,6 +27,8 @@ func chapterOne() {
     // Foster Thompson
     //Chapter 1
     //Optimism about reality
+    
+    
     if Spy {
        Job = "spy"
     } else {
@@ -37,10 +39,17 @@ func chapterOne() {
     print("He wasn’t necessarily a good spy, but he was noble and always tried his best.")
     print("On a rainy Monday, he entered the spy academy eagerly while soaking from the rain.")
     print("He opened the door and entered the elevator. He had forgot to press the floor button and was trapped in the elevator for \(MinutesLate) minutes")
-    list = listByAdding(item:"Still Waiting...", toList: list);
-    list += "\n" + "And Still Waiting...";
+    
+    print(elevatorFloorList.count)
+    //print("\(elevatorFloorList)")
+    print(elevatorFloorList[0])
+    print(elevatorFloorList[1])
+    print(elevatorFloorList[2])
+    
+    waitingList = listByAdding(item:"Still Waiting...", towaitingList: waitingList);
+    waitingList += "\n" + "And Still Waiting...";
 
-    print(list)
+    print(waitingList)
     print("until someone pressed the button to release him from his metal cage.");
     finishChapter()
 }
